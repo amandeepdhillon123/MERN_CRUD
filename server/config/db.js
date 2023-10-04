92635c72aa837e32da9e45408f329f7cd45cb33f
amandeepdhillon123 <amandeep24397@gmail.com>
2023-10-04 15:55:25 +0530

    completed

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 const dbConnect = ()=>{
-    mongoose.connect(process.env.MONGODB_URl,{
+    mongoose.connect(process.env.MONGODB_URL,{
         useNewUrlParser:true,
         useUnifiedTopology:true
     })
